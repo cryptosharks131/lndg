747a9de18b2822dfc48e73d537a385ce19070198
cryptosharks131 <cryptosharks131@gmail.com>
2021-04-07 11:20:25 -0400

    Add indexes to api output

--- gui/serializers.py
+++ gui/serializers.py
@@ -4,26 +4,31 @@ from .models import Payments, Invoices, Forwards, Channels, Rebalancer
 ##FUTURE UPDATE 'exclude' TO 'fields'
 
 class PaymentSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
     class Meta:
         model = Payments
         exclude = []
 
 class InvoiceSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
     class Meta:
         model = Invoices
         exclude = []
 
 class ForwardSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
     class Meta:
         model = Forwards
         exclude = []
 
 class ChannelSerializer(serializers.HyperlinkedModelSerializer):
+    chan_id = serializers.ReadOnlyField()
     class Meta:
         model = Channels
         exclude = []
 
 class RebalancerSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
     requested = serializers.ReadOnlyField()
     start = serializers.ReadOnlyField()
     stop = serializers.ReadOnlyField()
