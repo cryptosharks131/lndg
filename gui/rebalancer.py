3900985a1f3884bc87c94a801e08cbe337671d99
cryptosharks131 <cryptosharks131@gmail.com>
2021-05-28 15:10:16 -0400

    No longer run a new rebalance just because the outbound cans changed

--- gui/rebalancer.py
+++ gui/rebalancer.py
@@ -135,7 +135,7 @@ def auto_schedule():
                         target_fee = int(target_value * (1 / value_per_fee))
                         if Rebalancer.objects.filter(last_hop_pubkey=inbound_pubkey.remote_pubkey).exclude(status=0).exists():
                             last_rebalance = Rebalancer.objects.filter(last_hop_pubkey=inbound_pubkey.remote_pubkey).exclude(status=0).order_by('-id')[0]
-                            if last_rebalance.outgoing_chan_ids != str(outbound_cans) or last_rebalance.value != target_value or last_rebalance.status in [2, 6] or (last_rebalance.status in [3, 4] and (int((datetime.now() - last_rebalance.stop).total_seconds() / 60) > 30)):
+                            if last_rebalance.value != target_value or last_rebalance.status in [2, 6] or (last_rebalance.status in [3, 4] and (int((datetime.now() - last_rebalance.stop).total_seconds() / 60) > 30)):
                                 print('Creating Auto Rebalance Request')
                                 print('Request for:', target.chan_id)
                                 print('Request routing through:', outbound_cans)
