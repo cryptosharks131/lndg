9cff64428e7ee4657ed949b53dbc39d37e4ad2bc
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2022-01-13 14:05:16 -0500

    Fix missing save for cost_to

--- jobs.py
+++ jobs.py
@@ -35,11 +35,14 @@ def update_payments(stub):
                     if attempt.status == 1:
                         hops = attempt.route.hops
                         hop_count = 0
+                        cost_to = 0
                         total_hops = len(hops)
                         for hop in hops:
                             hop_count += 1
                             alias = stub.GetNodeInfo(ln.NodeInfoRequest(pub_key=hop.pub_key, include_channels=False)).node.alias
-                            PaymentHops(payment_hash=new_payment, attempt_id=attempt.attempt_id, step=hop_count, chan_id=hop.chan_id, alias=alias, chan_capacity=hop.chan_capacity, node_pubkey=hop.pub_key, amt=round(hop.amt_to_forward_msat/1000, 3), fee=round(hop.fee_msat/1000, 3)).save()
+                            fee = hop.fee_msat/1000
+                            PaymentHops(payment_hash=new_payment, attempt_id=attempt.attempt_id, step=hop_count, chan_id=hop.chan_id, alias=alias, chan_capacity=hop.chan_capacity, node_pubkey=hop.pub_key, amt=round(hop.amt_to_forward_msat/1000, 3), fee=round(fee, 3), cost_to=round(cost_to, 3)).save()
+                            cost_to += fee
                             if hop_count == 1:
                                 new_payment.chan_out = hop.chan_id
                                 new_payment.chan_out_alias = alias
