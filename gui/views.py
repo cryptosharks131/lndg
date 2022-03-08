32c4e7ceac1124c371f4d0454b1fe51fc82bd70b
cryptosharks131 <cryptosharks131@gmail.com>
2022-03-07 21:49:38 -0500

    Rebalancer requies an outgoing route, additional page links

--- gui/views.py
+++ gui/views.py
@@ -1093,10 +1093,13 @@ def rebalance(request):
                     chan_ids = []
                     for channel in form.cleaned_data['outgoing_chan_ids']:
                         chan_ids.append(channel.chan_id)
-                    target_alias = Channels.objects.filter(is_active=True, is_open=True, remote_pubkey=form.cleaned_data['last_hop_pubkey'])[0].alias if Channels.objects.filter(is_active=True, is_open=True, remote_pubkey=form.cleaned_data['last_hop_pubkey']).exists() else ''
-                    fee_limit = 0 if form.cleaned_data['value'] == 0 else int(form.cleaned_data['fee_limit']*form.cleaned_data['value']*0.000001)
-                    Rebalancer(value=form.cleaned_data['value'], fee_limit=fee_limit, outgoing_chan_ids=chan_ids, last_hop_pubkey=form.cleaned_data['last_hop_pubkey'], target_alias=target_alias, duration=form.cleaned_data['duration']).save()
-                    messages.success(request, 'Rebalancer request created!')
+                    if len(chan_ids) > 0:
+                        target_alias = Channels.objects.filter(is_active=True, is_open=True, remote_pubkey=form.cleaned_data['last_hop_pubkey'])[0].alias if Channels.objects.filter(is_active=True, is_open=True, remote_pubkey=form.cleaned_data['last_hop_pubkey']).exists() else ''
+                        fee_limit = 0 if form.cleaned_data['value'] == 0 else int(form.cleaned_data['fee_limit']*form.cleaned_data['value']*0.000001)
+                        Rebalancer(value=form.cleaned_data['value'], fee_limit=fee_limit, outgoing_chan_ids=chan_ids, last_hop_pubkey=form.cleaned_data['last_hop_pubkey'], target_alias=target_alias, duration=form.cleaned_data['duration']).save()
+                        messages.success(request, 'Rebalancer request created!')
+                    else:
+                        messages.error(request, 'You must select atleast one outgoing channel.')
                 else:
                     messages.error(request, 'Target peer is invalid or unknown.')
             except Exception as e:
