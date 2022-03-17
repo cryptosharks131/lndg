432b5bbfa02f8e36a2371b1704359c99a582b005
cryptosharks131 <cryptosharks131@gmail.com>
2022-03-17 00:35:24 -0400

    Update both channels after success

--- rebalancer.py
+++ rebalancer.py
@@ -37,6 +37,7 @@ def run_rebalancer(rebalance):
             elif payment_response.status == 2:
                 #SUCCESSFUL
                 rebalance.status = 2
+                successful_out = payment_response.htlcs[0].route.hops[0].pub_key
             elif payment_response.status == 3:
                 #FAILURE
                 if payment_response.failure_reason == 1:
@@ -67,7 +68,7 @@ def run_rebalancer(rebalance):
         rebalance.stop = datetime.now()
         rebalance.save()
         if rebalance.status == 2:
-            update_channel(stub, rebalance.last_hop_pubkey)
+            update_channels(stub, rebalance.last_hop_pubkey, successful_out)
             auto_rebalance_channels = Channels.objects.filter(is_active=True, is_open=True, private=False).annotate(percent_outbound=((Sum('local_balance')+Sum('pending_outbound'))*100)/Sum('capacity')).annotate(inbound_can=(((Sum('remote_balance')+Sum('pending_inbound'))*100)/Sum('capacity'))/Sum('ar_in_target'))
             inbound_cans = auto_rebalance_channels.filter(remote_pubkey=rebalance.last_hop_pubkey).filter(auto_rebalance=True, inbound_can__gte=1)
             if len(inbound_cans) > 0:
@@ -80,8 +81,15 @@ def run_rebalancer(rebalance):
             next_rebalance = None
         return next_rebalance
 
-def update_channel(stub, peer_pubkey):
-    channel = stub.ListChannels(ln.ListChannelsRequest(peer=bytes.fromhex(peer_pubkey))).channels[0]
+def update_channels(stub, incoming_channel, outgoing_channel):
+    # Incoming channel update
+    channel = stub.ListChannels(ln.ListChannelsRequest(peer=bytes.fromhex(incoming_channel))).channels[0]
+    db_channel = Channels.objects.filter(chan_id=channel.chan_id)[0]
+    db_channel.local_balance = channel.local_balance
+    db_channel.remote_balance = channel.remote_balance
+    db_channel.save()
+    # Outgoing channel update
+    channel = stub.ListChannels(ln.ListChannelsRequest(peer=bytes.fromhex(outgoing_channel))).channels[0]
     db_channel = Channels.objects.filter(chan_id=channel.chan_id)[0]
     db_channel.local_balance = channel.local_balance
     db_channel.remote_balance = channel.remote_balance
