be666c20fb7a4afcb8234bb7fd4dfe9c27098c83
cryptosharks131 <cryptosharks131@gmail.com>
2022-06-23 09:07:16 -0400

    Remove testing block

--- gui/views.py
+++ gui/views.py
@@ -1043,8 +1043,8 @@ def actions(request):
 @login_required(login_url='/lndg-admin/login/?next=/')
 def pending_htlcs(request):
     if request.method == 'GET':
-        #stub = lnrpc.LightningStub(lnd_connect(settings.LND_DIR_PATH, settings.LND_NETWORK, settings.LND_RPC_SERVER))
-        block_height = 122325165 #stub.GetInfo(ln.GetInfoRequest()).block_height
+        stub = lnrpc.LightningStub(lnd_connect(settings.LND_DIR_PATH, settings.LND_NETWORK, settings.LND_RPC_SERVER))
+        block_height = stub.GetInfo(ln.GetInfoRequest()).block_height
         context = {
             'incoming_htlcs': PendingHTLCs.objects.filter(incoming=True).annotate(blocks_til_expiration=Sum('expiration_height')-block_height).annotate(hours_til_expiration=((Sum('expiration_height')-block_height)*10)/60).order_by('hash_lock'),
             'outgoing_htlcs': PendingHTLCs.objects.filter(incoming=False).annotate(blocks_til_expiration=Sum('expiration_height')-block_height).annotate(hours_til_expiration=((Sum('expiration_height')-block_height)*10)/60).order_by('hash_lock')
