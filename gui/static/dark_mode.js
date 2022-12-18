b5adc4b6b7fa7d9427da2448ec286286da976e7a
cryptosharks131 <cryptosharks131@gmail.com>
2022-12-18 14:09:16 -0500

    Add more sorting and test a dark mode toggle

---
+++ gui/static/dark_mode.js
@@ -0,0 +1,37 @@
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+function darkMode() {
+    let darkmode = getCookie("darkmode");
+    if (darkmode != "") {
+        if (darkmode == "true") {
+          document.cookie = "darkmode=false";
+        } else {
+          document.cookie = "darkmode=true";
+        }
+    } else {
+        document.cookie = "darkmode=true"; 
+    }
+    var element = document.body;
+    element.classList.toggle("dark-mode");  
+}
+
+function checkDarkMode() {
+    let darkmode = getCookie("darkmode");
+    if (darkmode == "true") {
+        var element = document.body;
+        element.classList.toggle("dark-mode"); 
+    }
+}
