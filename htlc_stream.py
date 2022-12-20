72431feea699ab74f22bd2a2f2a1e81a377d2725
Yuya Ogawa <yuya.ogw@gmail.com>
2022-12-20 09:35:28 +0900

    Add check key existence

--- htlc_stream.py
+++ htlc_stream.py
@@ -35,24 +35,26 @@ def main():
             elif response.event_type == 3 and str(response.settle_event) != '':
                 # Delete forward_event
                 key = str(response.incoming_channel_id) + str(response.outgoing_channel_id) + str(response.incoming_htlc_id) + str(response.outgoing_htlc_id)
-                del all_forwards[key]
+                if key in all_forwards.keys():
+                        del all_forwards[key]
             elif response.event_type == 3 and str(response.forward_fail_event) == '':
                 key = str(response.incoming_channel_id) + str(response.outgoing_channel_id) + str(response.incoming_htlc_id) + str(response.outgoing_htlc_id)
-                forward_event = all_forwards[key]
-                in_chan_id = response.incoming_channel_id
-                out_chan_id = response.outgoing_channel_id
-                in_chan = Channels.objects.filter(chan_id=in_chan_id)[0] if Channels.objects.filter(chan_id=in_chan_id).exists() else None
-                out_chan = Channels.objects.filter(chan_id=out_chan_id)[0] if Channels.objects.filter(chan_id=out_chan_id).exists() else None
-                in_chan_alias = in_chan.alias if in_chan is not None else None
-                out_chan_alias = out_chan.alias if out_chan is not None else None
-                out_chan_liq = out_chan.local_balance if out_chan is not None else None
-                out_chan_pending = out_chan.pending_outbound if out_chan is not None else None
-                amount = (forward_event.info.incoming_amt_msat/1000)
-                wire_failure = 99
-                failure_detail = 99
-                missed_fee = 0
-                FailedHTLCs(amount=amount, chan_id_in=in_chan_id, chan_id_out=out_chan_id, chan_in_alias=in_chan_alias, chan_out_alias=out_chan_alias, chan_out_liq=out_chan_liq, chan_out_pending=out_chan_pending, wire_failure=wire_failure, failure_detail=failure_detail, missed_fee=missed_fee).save()
-                del all_forwards[key]
+                if key in all_forwards.keys():
+                    forward_event = all_forwards[key]
+                    in_chan_id = response.incoming_channel_id
+                    out_chan_id = response.outgoing_channel_id
+                    in_chan = Channels.objects.filter(chan_id=in_chan_id)[0] if Channels.objects.filter(chan_id=in_chan_id).exists() else None
+                    out_chan = Channels.objects.filter(chan_id=out_chan_id)[0] if Channels.objects.filter(chan_id=out_chan_id).exists() else None
+                    in_chan_alias = in_chan.alias if in_chan is not None else None
+                    out_chan_alias = out_chan.alias if out_chan is not None else None
+                    out_chan_liq = out_chan.local_balance if out_chan is not None else None
+                    out_chan_pending = out_chan.pending_outbound if out_chan is not None else None
+                    amount = (forward_event.info.incoming_amt_msat/1000)
+                    wire_failure = 99
+                    failure_detail = 99
+                    missed_fee = 0
+                    FailedHTLCs(amount=amount, chan_id_in=in_chan_id, chan_id_out=out_chan_id, chan_in_alias=in_chan_alias, chan_out_alias=out_chan_alias, chan_out_liq=out_chan_liq, chan_out_pending=out_chan_pending, wire_failure=wire_failure, failure_detail=failure_detail, missed_fee=missed_fee).save()
+                    del all_forwards[key]
     except Exception as e:
         print('Error while running failed HTLC stream: ' + str(e))
         sleep(20)
