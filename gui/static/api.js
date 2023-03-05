df01b7cbcf2747e5152e4fae442f6ac623fbfa5f
cryptosharks131 <cryptosharks131@gmail.com>
2023-03-04 22:29:04 -0500

    Flash input box when api updated

--- gui/static/api.js
+++ gui/static/api.js
@@ -18,3 +18,34 @@ function call(method, urlExt, sendData={}, params={}, callback=null){
     if(method != 'GET') request_data['body'] = JSON.stringify(sendData);
     fetch('api' + urlExt, request_data).then(response => response.json()).then(result => {if(callback != null) callback(result)});
 }
+
+function flash(element){
+    var rgb = window.getComputedStyle(element).backgroundColor;
+    rgb = rgb.substring(4, rgb.length-1).replace(/ /g, '').split(',');
+    var r = rgb[0], g = rgb[1], bOrigin = rgb[2], b = bOrigin;
+    var add = false;
+    var complete = false;
+    const increment = 15;
+    var flashOn = setInterval(() => {
+        if(add){
+            if(b < 255 && (b+increment) <= 255){
+                b += increment;
+            }else{
+                add = false;
+                complete = true;
+            }
+        }else{
+            if(complete == true && b < bOrigin){
+                b = bOrigin;
+                clearInterval(flashOn);
+            }
+            else if(b > 0 && (b-increment) >= 0){
+                b -= increment;
+            }else{
+                add = true;
+            }
+        }
+        element.style.backgroundColor = 'RGB('+r+','+g+','+b+')';
+        if(b == bOrigin) element.style.removeProperty("background-color");
+    }, 50);
+}
